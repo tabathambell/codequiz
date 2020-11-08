8c3862d8c862382476c68c9ce30b50abfaf152e6
Tabatha Bell <tabathambell@live.com>
2020-11-07 22:05:48 -0500

    modified timer, arrays, and for loops. added some html to modify page to make iet more consistent

--- script/script.js
+++ script/script.js
@@ -15,111 +15,117 @@
 
 */
 
-var content = document.querySelector(".content");
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
-var startBtn = document.getElementById('start')
-
-function welcomePage() {
-    var welcomeText = document.createElement("p");
-    welcomeText.innerText = "Hello and welcome to this quiz! You have 60 seconds to complete the following 5 questions. Good luck and have fun!";
-    var btnStart = document.createElement("button");
-    btnStart.innerText = "Start Quiz!";
-    content.appendChild(welcomeText);
-    content.appendChild(btnStart);
-};
-
 // The array of questions for the game.
 var questions = [
-    { question1: 'The sky is blue.', 
+    { question: 'The sky is blue.', 
     option1: 'here',
     option2: 'here',
     option3: 'here',
     option4: 'here',
-    answer1: 'here' },
-    { question2: 'The sky is blue.', 
+    answer: 1 },
+    { question: 'The sky is blue.', 
     option1: 'here',
     option2: 'here',
     option3: 'here',
     option4: 'here',
-    answer2: 'here' },
-    { question3: 'The sky is blue.', 
+    answer: 1 },
+    { question: 'The sky is blue.', 
     option1: 'here',
     option2: 'here',
     option3: 'here',
     option4: 'here',
-    answer3: 'here' },
-    { question4: 'The sky is blue.', 
+    answer: 1 },
+    { question: 'The sky is blue.', 
     option1: 'here',
     option2: 'here',
     option3: 'here',
     option4: 'here',
-    answer4: 'here' },
-    { question5: 'The sky is blue.', 
+    answer: 1 },
+    { question: 'The sky is blue.', 
     option1: 'here',
     option2: 'here',
     option3: 'here',
     option4: 'here',
-    answer5: 'here' },
+    answer: 1 },
   ];
+
+  // Put DOM elements here
+var timerEl = document.getElementById('countdown');
+var goBtn = document.getElementById('go');
+var introP = document.getElementById('intro');
+var quizQEl = document.getElementById('quiz-questions');
+var quizSEl = document.getElementById('quiz-status');
+
+var a1Btn = document.getElementById('option1');
+var a2Btn = document.getElementById('option2');
+var a3Btn = document.getElementById('option3');
+var a4Btn = document.getElementById('option4');
   
-  var score = 0;
-  for (var i = 0; i < questions.length; i++) {
-    
-    var answer = confirm(questions[i].question);
-    // Compare answers !!!!!!!THIS IS THE ONE I NEED HELP WITH!!!!!
-    if (
-      I DON'T KNOW WHAT I'M DOING
-    ) {
-      score++;
-      alert('Correct!');
-    } else {
-      alert('Wrong!');
+var score = 0;
+var currentQuestion = -1;
+
+// This is the run when the page loads.
+function welcomePage() {
+  quizQEl.setAttribute("style","display: none;");
+};
+
+// Function for moving to the next question, and handling the beginning and end-states of the quiz
+function nextQuestion() {
+  currentQuestion++;
+  if (currentQuestion >= questions.length) {
+    quizQEl.setAttribute("style", "display: none;");
+    introP.setAttribute("style", "display: default;");
+    quizSEl.innerText = "THE JIG IS UP";
+    introP.innerText = "You got " + score + " out of a possible " + questions.length +
+      " questions correct! Refresh the page the play again!";
+  } else {
+    if (currentQuestion <= 0) {
+      introP.setAttribute("style","display: none;");
+      goBtn.setAttribute("style","display: none");
+      quizQEl.setAttribute("style","display: default;");
+      countdown();
     }
+  
+    quizSEl.innerText "Question " + (currentQuestion + 1) + ": " + questions[currentQuestion].question;
+    a1Btn.innerText = questions[currentQuestion].option1;
+    a2Btn.innerText = questions[currentQuestion].option2;
+    a3Btn.innerText = questions[currentQuestion].option3;
+    a4Btn.innerText = questions[currentQuestion].option4;
   }
- 
-  alert('THE JIG IS UP. GAME OVER.');
-  alert('You received ' + score + 'out of' + questions.length);
-  return;
-
-for (i = 0; i < questions.length; i++) {
-    var head = document.createElement("h2");
-    head.innerText = questions[i][0];
-    content.appendChild(head);
-    
-    for(j = 0; j < questions[i].length; j++) {
-        var element = document.createElement("p");
-        element.innerText = questions[i][j];
-        content.appendChild(element);
-    }
 }
 
-function countdown() {
-  var timeLeft = 60;
-
-  var timeInterval = setInterval(function() {
-    if(timeLeft <= 0){
-      clearInterval(timeInterval);
-      timerEl.textContent = "Done";
-      displayMessage();
-    } else {
-      timerEl.textContent = timeLeft + " seconds left";
-    }
-    timeLeft -= 1;
-  }, 1000);
+// These four functions are bound to the answer buttons.
+// They just call the generic checkAnswer () function.
+function checkAnswer1() {
+  checkAnswer(1);
 }
 
-  var msgInterval = setInterval(function() {
-    if (words[wordCount] === undefined) {
-      clearInterval(msgInterval);
-    } else {
-      mainEl.textContent = words[wordCount];
-      wordCount++;
-    }
-  }, 300);
+function checkAnswer2() {
+  checkAnswer(2);
 }
 
-startBtn.onclick = countdown;
+function checkAnswer3() {
+  checkAnswer(3);
+}
+
+function checkAnswer4() {
+  checkAnswer(4);
+}
+
+// Checks whether the chosen answer is the correct one and updates the quiz
+function checkAnswer(num) {
+  var correct = questions[currentQuestion].answer;
+  if (correct == num) {
+    score++;
+    alert("Correct! " + score + " correct out of " + (currentQuestion + 1) + "questions so far!");
+  } else {
+    alert("Incorrect! " + score + " correct out of " + (currentQuestion + 1) + " questions so far! ");
+  }
+nextQuestion();
+}
+
+// sets up the timer and updates it at 1 second intervals. Returns if it hits 0.
+function countdown();
+
 // welcome page call
 welcomePage();
-button.addEventListener("onMouseUp", function);
